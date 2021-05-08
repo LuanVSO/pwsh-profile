30f58a5bd56331c8da3a4f11011e1216e018ead8
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2021-05-08 19:13:24 -0300

    set tab size to 4

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,6 +13,13 @@ if ($env:WT_SESSION) {
 	}
 }
 
+# same as tabs 4
+Write-host `e[?25l`e[3g`r`eH -NoNewline
+for ($i = 0; $i -lt [math]::Floor([System.Console]::BufferWidth / 4); $i++) {
+	Write-Host "    `eH" -NoNewline
+}
+Write-Host `e[?25h`r -NoNewline
+
 # workaround for https://github.com/git-for-windows/git/issues/3177
 Set-Item env:\TERM -Value "xterm-256color"
 
