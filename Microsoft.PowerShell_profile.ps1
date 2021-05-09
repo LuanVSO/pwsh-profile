7f71afb195090ada9a4aedfbc479cb62df20fd69
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2021-05-08 22:32:39 -0300

    cleanuo prompt string

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -6,10 +6,7 @@ if ($env:WT_SESSION) {
 		if ($pwd.provider.name -eq "FileSystem") {
 			Write-host "`e]9;9;`"$p`"`e\" -NoNewline
 		}
-		"`e[0mPS $($p)$('>' * ($nestedPromptLevel + 1)) ";
-		# .Link
-		# https://go.microsoft.com/fwlink/?LinkID=225750
-		# .ExternalHelp System.Management.Automation.dll-help.xml
+		"`e[0mPS $p$('>' * ($nestedPromptLevel + 1)) ";
 	}
 }
 
