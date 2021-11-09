522cfb494753e1e79669de2c15252eec977257f7
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2021-11-09 18:54:54 -0300

    update completer

--- completions/winget.ps1
+++ completions/winget.ps1
@@ -3,6 +3,7 @@ using namespace System.Management.Automation.Language
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
 	param($wordToComplete, $commandAst, $cursorPosition)
+	[Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
 	$Local:word = $wordToComplete.Replace('"', '""')
 	$Local:ast = $commandAst.ToString().Replace('"', '""')
 	winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
