2911409f2a3d871bf62b235ee0444e04619473c0
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2021-12-20 18:26:11 -0300

    add more reset codes to reset

--- profile.ps1
+++ profile.ps1
@@ -65,7 +65,7 @@ function which($c) {
 }
 function take([string]$path) { mkdir $path ; Set-Location $path }
 
-function reset { write-host "`ec" -NoNewline }
+function reset { write-host "`ec`e]104`a`e[!p`e[?3;4l`e[4l`e>`e[?69l" -NoNewline }
 
 function tabs($tabsize) {
 	Write-host "`e[?25l`e[3g`r`eH" -NoNewline
